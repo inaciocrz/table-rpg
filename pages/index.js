0721360032fd6d059223718014345d31a0af79ca
Inacio Campos <inacio.camposrezende@gmail.com>
2022-04-16 12:47:21 -0300

    first build

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Unity, { UnityContext } from "react-unity-webgl";
+
 export default function Home() {
+  const unityContext = new UnityContext({
+    loaderUrl: "./Build/TableTopRPG.loader.js",
+    dataUrl: "./Build/TableTopRPG.data",
+    frameworkUrl: "./Build/TableTopRPG.framework.js",
+    codeUrl: "./Build/TableTopRPG.wasm",
+  });
   return (
-    <>Hello World</>
-  )
+    <>
+      <Unity unityContext={unityContext} style={{width: 480, height: 320}}/>
+      <button onClick={()=>{unityContext.setFullscreen(true)}}>FullScreen</button>
+    </>
+  );
 }
